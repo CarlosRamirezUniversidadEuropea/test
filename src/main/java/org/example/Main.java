1dc886c88927e9554ea57ea3ef6a307d4c82f217
carlosramirezlizan <carlos.ramirez2@universidadeuropea.es>
2024-10-15 01:03:05 +0200

    Spring app

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,29 +5,31 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        System.out.println("Starting app...");
+        while (true) {
+            System.out.println("Hello world!");
+            System.out.println("Starting app...");
 
-        try {
-            Thread.sleep(2000);
-            System.out.println("Welcome to my Java app!");
-            Thread.sleep(2000);
-            System.out.println("Would you like to proceed? (yes/no)");
-            // Simulate user input
-            Thread.sleep(2000);
-            System.out.println("yes");
-            Thread.sleep(2000);
-            System.out.println("Great! I will create the order for you with the details.");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(2000);
+                System.out.println("Welcome to my Java app!");
+                Thread.sleep(2000);
+                System.out.println("Would you like to proceed? (yes/no)");
+                // Simulate user input
+                Thread.sleep(2000);
+                System.out.println("yes");
+                Thread.sleep(2000);
+                System.out.println("Great! I will create the order for you with the details.");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            List<String> items = new ArrayList<>();
+            items.add("Item1");
+            items.add("Item2");
+
+            Order pedido = new Order("Alice", "Perez", CustomerType.VIP, items, 1200);
+            pedido.printOrderDetails();
         }
-
-        List<String> items = new ArrayList<>();
-        items.add("Item1");
-        items.add("Item2");
-
-        Order pedido = new Order("Alice","Perez", CustomerType.VIP, items, 1200);
-        pedido.printOrderDetails();
     }
 }
